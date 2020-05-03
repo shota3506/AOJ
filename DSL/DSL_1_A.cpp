87ccaff81fd7f40e7d565a7861a3874524426392
shota3506 <s.shota.710.3506@gmail.com>
2020-05-03 11:04:40 +0900

    Advanced data structure

--- DSL/DSL_1_A.cpp
+++ DSL/DSL_1_A.cpp
@@ -5,40 +5,34 @@
 using namespace std;
 
 class UnionFindTree {
-public:
-    explicit UnionFindTree(int size);
-    void unite(int x, int y);
-    bool same(int x, int y);
-    int findSet(int x);
 private:
     vector<int> p, rank;
+public:
+    UnionFindTree(int size) {
+        p = vector<int>(size);
+        rank = vector<int>(size,0);
+        for(int i=0; i<size; i++) p[i] = i;
+    }
+
+    int findSet(int x) {
+        if(p[x] != x) p[x] = findSet(p[x]);
+        return p[x];
+    }
+
+    void unite(int x, int y) {
+        int sx = findSet(x), sy = findSet(y);
+        if(rank[sx] > rank[sy]) p[sy] = sx;
+        else {
+            p[sx] = sy;
+            if(rank[sx] == rank[sy]) rank[sx]++;
+        }
+
+    }
+
+    bool same(int x, int y) {
+        return findSet(x) == findSet(y);
+    }
 };
-
-UnionFindTree::UnionFindTree(int size) {
-    p = vector<int>(size);
-    rank = vector<int>(size, 0);
-    for(int i=0; i<size; i++) p[i] = i;
-}
-
-void UnionFindTree::unite(int x, int y) {
-    int sx = findSet(x), sy = findSet(y);
-    if(rank[sx] > rank[sy]) {
-        p[sy] = sx;
-    }
-    else {
-        p[sx] = sy;
-        if(rank[sx] == rank[sy]) rank[sy]++;
-    }
-}
-
-bool UnionFindTree::same(int x, int y) {
-    return findSet(x) == findSet(y);
-}
-
-int UnionFindTree::findSet(int x) {
-    if(x != p[x]) p[x] = findSet(p[x]);
-    return p[x];
-}
 
 int main() {
     int n, q, com, x, y;
@@ -46,7 +40,9 @@ int main() {
     UnionFindTree S(n);
     for(int i = 0; i < q; i++) {
         cin >> com >> x >> y;
-        if(com == 0) S.unite(x, y);
+        if(com == 0) {
+            S.unite(x, y);
+        }
         else if(com == 1) {
             if(S.same(x, y)) cout << 1 << endl;
             else cout << 0 << endl;
--- DSL/DSL_2_A.cpp
+++ DSL/DSL_2_A.cpp
@@ -1,49 +1,43 @@
 //Segment Tree
 #include <iostream>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
 int INIT = 2147483647;
 
 class SegmentTree {
-public:
-    explicit SegmentTree(int size);
-    void update(int i, int x);
-    int find(int s, int t);
-    int query(int s, int t, int k, int l, int r);
-private:
-    int n;
+    int size;
     vector<int> p;
-};
-
-SegmentTree::SegmentTree(int size) {
-    n = 1;
-    while(n<size) n*=2;
-    p = vector<int>(2*n-1, INIT);
-}
-
-void SegmentTree::update(int i, int x) {
-    int s = i+n-1;
-    p[s] = x;
-    while(s > 0) {
-        s = (s-1)/2;
-        p[s] = min(p[2*s+1], p[2*s+2]);
+public:
+    SegmentTree(int n) {
+        // 0-indexed
+        size = 1;
+        while(size<n) size *= 2;
+        p = vector<int>(2*size-1, INIT);
     }
-}
 
-int SegmentTree::find(int s, int t) {
-    return query(s, t, 0, 0, n);
-}
+    void update(int i, int x) {
+        int s = size+i-1;
+        p[s] = x;
+        while(s > 0) {
+            s = (s - 1) / 2;
+            p[s] = min(p[2*s+1], p[2*s+2]);
+        }
+    }
 
-int SegmentTree::query(int s, int t, int k, int l, int r) {
-    if(r <= s || t <= l) return INIT;
-    if(s <= l && r <= t) return p[k];
-    int lv = query(s, t, 2*k+1, l, (l+r)/2);
-    int rv = query(s, t, 2*k+2, (l+r)/2, r);
-    return min(lv, rv);
-}
+    int find(int s, int t) {
+        return query(s, t, 0, 0, size);
+    }
+
+    int query(int s, int t, int k, int l, int r) {
+        if(r<=s || t<=l) return INIT;
+        if(s<=l && r<=t) return p[k];
+        int lv = query(s, t, 2*k+1, l, (l+r)/2);
+        int rv = query(s, t, 2*k+2, (l+r)/2, r);
+        return min(lv, rv);
+    }
+};
 
 int main() {
     int n, q, com, x, y;
